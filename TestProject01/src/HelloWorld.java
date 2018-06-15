decc7f22f9bfee7118e60092b098d2ef6efa0344
matatabi96 <okunaga1988@yahoo.co.jp>
2018-06-15 21:49:32 +0900

    6月15日

--- TestProject01/src/HelloWorld.java
+++ TestProject01/src/HelloWorld.java
@@ -2,10 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int a = 10;
-		System.out.println(++a);
-		System.out.println(++a);
-		System.out.println(++a);
-		System.out.println(++a);
+
+		for (int i=1;i<=5;i++){
+			System.out.println("★");
+		}
+
 	}
 }
