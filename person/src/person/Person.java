32fe16147d1912030706516c2be62fd9e3417dbd
matatabi96 <okunaga1988@yahoo.co.jp>
2018-06-22 22:00:58 +0900

    0622金曜日

--- person/src/person/Person.java
+++ person/src/person/Person.java
@@ -1,22 +1,28 @@
 package person;
 
 public class Person {
-		public String name = null;
-		public int age = 0;
-		public String email = null;
-		public String phoneNumber = null;
-		public String phonecode = null;
-		public String Address = null;
+	public String name = null;
+	public int age =0;
 
-		public void talk(){
-			System.out.println(this.name+"が話す");
-		}
+	public Person(){}
 
-		public void walk(){
-			System.out.println(this.name+"が歩く");
-		}
+	public Person(String name,int age){
+		this.name =name;
+		this.age = age;
+	}
 
-		public void run(){
-			System.out.println(this.name+"が走る");
-		}
+	public Person(String name){
+		this.name=name;
+		this.age=0;
+	}
+
+	public Person(int age){
+		this.name="名前なし";
+		this.age=age;
+	}
+
+	public Person(int age,String name){
+		this.name=name;
+		this.age=age;
+	}
 }
